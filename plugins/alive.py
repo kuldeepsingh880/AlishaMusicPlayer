8414519f335eb69454588e416bce5019b34e12ea
khushbuxabhi <91168655+khushbuxabhi@users.noreply.github.com>
2021-12-27 11:22:03 +0530

    Update alive.py

--- plugins/alive.py
+++ plugins/alive.py
@@ -33,7 +33,7 @@ async def _human_time_duration(seconds):
 @Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
-        video=f"https://telegra.ph/file/8776c24d48fcafa605ddd.mp4",
+        photo=f"https://telegra.ph/file/fd880593afcb71a2c02cd.jpg",
         caption=f"""**━━━━━━━━━━━━━━━━━━━━━━━━
 💥 ʜᴇʟʟᴏ, ɪ ᴀᴍ ᴀʟɪsʜᴀ sᴜᴘᴇʀ ғᴀsᴛ ᴠᴄ ᴘʟᴀʏᴇʀ
 ʙᴏᴛ ғᴏʀ ᴛᴇʟᴇɢʀᴀᴍ ɢʀᴏᴜᴘs ...
